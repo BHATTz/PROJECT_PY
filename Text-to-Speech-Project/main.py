63a738ee92fb8252ae0b2563b3fc5c66f4326b2b
CODERx <sarthakbhatt0@gmail.com>
2023-07-19 19:03:55 +0530

    hello

--- Text-to-Speech-Project/main.py
+++ Text-to-Speech-Project/main.py
@@ -1,17 +1,17 @@
 import pyttsx3
 
 def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 120)  # Speed of speech
-    engine.setProperty('volume', 1.0)  # Volume (0.0 to 1.0)
-    engine.say(text)
-    engine.runAndWait()
+    engine = pyttsx3.init()  # Initialize the text-to-speech engine
+    engine.setProperty('rate', 90)  # Set the speed of speech (words per minute)
+    engine.setProperty('volume', 1.0)  # Set the volume (0.0 to 1.0)
+    engine.say(text)  # Queue the text to be spoken
+    engine.runAndWait()  # Speak the queued text
 
 # Example usage
 while True:
-    text = input("Enter something to hear (or 'q' to quit): ")
+    text = input("Enter something to hear (or 'q' to quit): ")  # Prompt the user for input
     if text == "q":
-        text_to_speech("Quitting the program. Goodbye!")
-        break
+        text_to_speech("Quitting the program. Goodbye!")  # Speak the goodbye message
+        break  # Exit the loop
 
-    text_to_speech(text)
+    text_to_speech(text)  # Speak the input text
