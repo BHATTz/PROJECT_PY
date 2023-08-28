c6edeebd41b9824e8fa28ccdf8fd41bd20671754
sarthak bhatt <sarthakbhatt0@gmail.com>
2023-08-28 18:33:57 +0530

    up

--- Text-to-Speech-Project/main.py
+++ Text-to-Speech-Project/main.py
@@ -1,3 +1,21 @@
+# import pyttsx3
+
+# def text_to_speech(text):
+#     engine = pyttsx3.init()  # Initialize the text-to-speech engine
+#     engine.setProperty('rate', 100)  # Set the speed of speech (words per minute)
+#     engine.setProperty('volume', 1.0)  # Set the volume (0.0 to 1.0)
+#     engine.say(text)  # Queue the text to be spoken
+#     engine.runAndWait()  # Speak the queued text
+
+# # Example usage
+# while True:
+#     text = input("Enter something to hear (or 'q' to quit): ")  # Prompt the user for input
+#     if text == "q":
+#         text_to_speech("Quitting the program. Goodbye!")  # Speak the goodbye message
+#         break  # Exit the loop
+
+#     text_to_speech(text)  # Speak the input text
+
 import pyttsx3
 
 def text_to_speech(text):
@@ -7,11 +25,24 @@ def text_to_speech(text):
     engine.say(text)  # Queue the text to be spoken
     engine.runAndWait()  # Speak the queued text
 
+def process_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            text_to_speech(content)
+    except FileNotFoundError:
+        print("File not found.")
+
 # Example usage
 while True:
-    text = input("Enter something to hear (or 'q' to quit): ")  # Prompt the user for input
-    if text == "q":
-        text_to_speech("Quitting the program. Goodbye!")  # Speak the goodbye message
-        break  # Exit the loop
-
-    text_to_speech(text)  # Speak the input text
+    choice = input("Enter 'f' to process a file, 'q' to quit: ")
+    
+    if choice == 'q':
+        text_to_speech("Quitting the program. Goodbye!")
+        break
+    
+    if choice == 'f':
+        file_path = input("Enter the path to the text file: ")
+        process_file(file_path)
+    else:
+        print("Invalid choice.")
--- translater/main.py
+++ translater/main.py
@@ -1,22 +1,25 @@
 from googletrans import Translator, LANGUAGES
 
-
 def translate_text(text, target_language):
     translator = Translator()
     translated = translator.translate(text, src="auto", dest=target_language)
     return translated.text
 
-
 def get_supported_languages():
     return LANGUAGES
 
-
 # Get user input
 input_text = input("Enter the text you want to translate: ")
-target_language = input("Enter the target language (e.g., 'es' for Spanish): ")
+target_language = input("Enter the target language code (e.g., 'es' for Spanish): ")
 
-if target_language in get_supported_languages():
+supported_languages = get_supported_languages()
+
+print("Supported languages:")
+for code, language in supported_languages.items():
+    print(f"{code}: {language}")
+
+if target_language in supported_languages:
     translated_text = translate_text(input_text, target_language)
     print(f"Translated text: {translated_text}")
 else:
-    print("Invalid target language. Please choose a valid language code.")
+    print("Invalid target language code. Please choose a valid language code.")
